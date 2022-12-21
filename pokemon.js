0b39dee600bbddf82350e0c0d9ab6b35c3cc37b2
baboda212 <113665619+baboda212@users.noreply.github.com>
2022-12-21 13:06:31 +0900

    Add files via upload

--- pokemon.js
+++ pokemon.js
@@ -23,5 +23,41 @@ const pokemons = [
       type: ['전기'],
       desc: "숲속에서 동료와 산다. 볼의 양쪽에 있는 전기 주머니에 전기를 모아 둔다."
     },
+    {
+      name: "버터플",
+      imgUrl: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/001201.png",
+      type: ['벌레','비행'],
+      desc: "꽃의 꿀을 매우 좋아한다. 약간의 꽃가루만으로 꽃밭이 있는 장소를 찾아낼 수 있다."
+    },
+    {
+      name: "구구",
+      imgUrl: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/001601.png",
+      type: ['노말','비행'],
+      desc: "싸움을 좋아하지 않는 얌전한 성격이지만 어설프게 건드리면 강력한 반격을 당하게 된다."
+    },
+    {
+      name: "꼬렛",
+      imgUrl: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/001901.png",
+      type: ['노말'],
+      desc: "어떤 장소에도 정착하여 살아갈 수 있는 생명력이 있다. 경계심이 매우 강하다."
+    },
+    {
+      name: "식스테일",
+      imgUrl: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/003701.png",
+      type: ['불꽃'],
+      desc: "불꽃의 구슬을 조종한다. 성장하면 6개의 꼬리는 끝이 갈라져서 한층 더 늘어난다."
+    },
+    {
+      name: "나옹",
+      imgUrl: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/005201.png",
+      type: ['노말'],
+      desc: "한밤중에 움직이는 습성이 있다. 반짝반짝 빛나는 것을 발견하면 그에 못지않게 눈동자가 반짝인다."
+    },
+    {
+      name: "고라파덕",
+      imgUrl: "https://data1.pokemonkorea.co.kr/newdata/pokedex/full/005401.png",
+      type: ['물'],
+      desc: "두통이 심해지면 이상한 힘을 사용하게 된다. 그때의 일은 기억하지 못한다."
+    },
   ];
   module.exports = pokemons;
